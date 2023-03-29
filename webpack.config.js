d5b7325e3f2e00e56819fc271d3189b65668a41b
Anna Baranovskaya <anya.baranovskaya@gmail.com>
2023-03-29 23:47:56 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.(js)$/, use: 'babel-loader' }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    }
+}
